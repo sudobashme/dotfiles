c54be2696d0e0352408ac2ae19d102b42a812843
sudobashme <6994597+sudobashme@users.noreply.github.com>
2026-06-25 00:43:08 -0500

    reordered rc.d

--- zsh/rc.d/450_zdharma.zsh
+++ zsh/rc.d/450_zdharma.zsh
@@ -1,10 +1,23 @@
 #!/usr/bin/env zsh
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #               repo: https://github.com/sudobashme/dotfiles
-#               file: 200_zbrowse.zsh
-#           filepath: ${ZDOTDIR}/rc.d/200_zbrowse.zsh
+#               file: 185_zsh_navigation_tools.zsh
+#           filepath: ${ZDOTDIR}/rc.d/185_zsh_navigation_tools.zsh
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+
+# zsh-navigation-tools (n-cd, n-kill, n-history, etc.)
+# A set of TUI helpers. Quite old but some people love the n-* commands.
+# These add noticeable startup cost. Disable with: export NO_ZSH_NAV_TOOLS=1
+if [[ -z $NO_ZSH_NAV_TOOLS ]]; then
+    fpath+=( ${ZDOTDIR}/plugins/zsh-navigation-tools )
+    source "${ZDOTDIR}/plugins/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh"
+
+    autoload n-list n-cd n-env n-kill n-panelize n-options n-aliases n-functions n-history n-help
+
+    alias naliases=n-aliases ncd=n-cd nenv=n-env nfunctions=n-functions nhistory=n-history
+    alias nkill=n-kill noptions=n-options npanelize=n-panelize nhelp=n-help
+fi
 # zui + zbrowse are supporting libraries for zsh-navigation-tools.
 # We skip them entirely if navigation tools are disabled.
 if [[ -n $NO_ZSH_NAV_TOOLS ]]; then
@@ -45,6 +58,9 @@ else
 fi
 unset __local_zplug __plugin_dir
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# nvim: ft=zsh
+# EOF
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # nvim: ft=zsh
 # EOF
